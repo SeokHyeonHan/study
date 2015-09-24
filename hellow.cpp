cc4b117ee0c85b66959741ff37fd18fe4c9110c0
shhan <classic226@naver.com>
2015-09-24 15:42:52 +0900

    wirte hellow world

---
+++ hellow.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout <<"hellow world"<<endl;
+	cout <<"Your world"<<endl;
+	cout <<"Your world"<<endl;
+	cout <<"tell Your world"<<endl;
+	return 0;
+}
